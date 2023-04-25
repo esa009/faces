d440ebbc36a918b4ee9b15876be8f94c561d7bef
esa009 <39069772+esa009@users.noreply.github.com>
2023-04-25 17:20:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,114 +1,168 @@
-import calendar  # Core Python Module
-from datetime import datetime  # Core Python Module
+from deepface import DeepFace
+import matplotlib.pyplot as plt
+import pandas as pd
+from PIL import Image
+import io
 
-import streamlit as st  # pip install streamlit
-from streamlit_option_menu import option_menu  # pip install streamlit-option-menu
 
-import database as db  # local import
 
-# -------------- SETTINGS --------------
-incomes = ["Salary", "Blog", "Other Income"]
-expenses = ["Rent", "Utilities", "Groceries", "Car", "Other Expenses", "Saving"]
-currency = "USD"
-page_title = "Income and Expense Tracker"
-layout = "centered"
-# --------------------------------------
-
+def img(image_1, image_2):
+  image1 = Image.open(io.BytesIO(image_1.get_bytes()))
+  image2 = Image.open(io.BytesIO(image_2.get_bytes()))
+  image1.save("test.png")
+  #image2.save("test2.png")
+  #return DeepFace.analyze("/content/test.png", enforce_detection=False)
+  return DeepFace.verify("/content/test.png", "/content/test2.png", enforce_detection=False)
 st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
 st.title(page_title + " " + page_icon)
 
-# --- DROP DOWN VALUES FOR SELECTING THE PERIOD ---
-years = [datetime.today().year, datetime.today().year + 1]
-months = list(calendar.month_name[1:])
+
+!rm /content/drive/MyDrive/anvilfaces/representations_vgg_face.pkl
+def compare(image_2):
+  image2 = Image.open(io.BytesIO(image_2.get_bytes()))
+  image2.save("test2.png")
+  result = DeepFace.find(img_path = '/content/test2.png', db_path = '/content/drive/MyDrive/anvilfaces', enforce_detection=False)
+  text1 = ""
+  for i in range(len(result.values)):
+   text1 += str(result.identity[i])[34:-4] + "\n"
+  return text1
 
 
-# --- DATABASE INTERFACE ---
-def get_all_periods():
-    items = db.fetch_all_periods()
-    periods = [item["key"] for item in items]
-    return periods
+!rm /content/drive/MyDrive/anvilfaces/representations_vgg_face.pkl
+result = ""
+def compare2(image_2):
+  image2 = Image.open(io.BytesIO(image_2.get_bytes()))
+  image2.save("test2.png")
+  result = DeepFace.find(img_path = '/content/test2.png', db_path = '/content/drive/MyDrive/anvilfaces', enforce_detection=False)
+  text = ""
+  for i in range(len(result.values)):
+    text += str(result.values[i])[-19:-1] + "\n"
+  return text  
+
+!rm /content/drive/MyDrive/anvilfaces/representations_vgg_face.pkl
+result = ""
+def compare3(image_2):
+  image2 = Image.open(io.BytesIO(image_2.get_bytes()))
+  image2.save("test2.png")
+  result = DeepFace.find(img_path = '/content/test2.png', db_path = '/content/drive/MyDrive/anvilfaces', enforce_detection=False)
+  text2 = ""
+  text2 = result.identity[0][34:-4]
+  return text2
+     
 
 
-# --- HIDE STREAMLIT STYLE ---
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
-
-# --- NAVIGATION MENU ---
-selected = option_menu(
-    menu_title=None,
-    options=["Data Entry", "Data Visualization"],
-    orientation="horizontal",
-)
-
-# --- INPUT & SAVE PERIODS ---
-if selected == "Data Entry":
-    st.header(f"Data Entry in {currency}")
-    with st.form("entry_form", clear_on_submit=True):
-        col1, col2 = st.columns(2)
-        col1.selectbox("Select Month:", months, key="month")
-        col2.selectbox("Select Year:", years, key="year")
-
-        "---"
-        with st.expander("Income"):
-            for income in incomes:
-                st.number_input(f"{income}:", min_value=0, format="%i", step=10, key=income)
-        with st.expander("Expenses"):
-            for expense in expenses:
-                st.number_input(f"{expense}:", min_value=0, format="%i", step=10, key=expense)
-        with st.expander("Comment"):
-            comment = st.text_area("", placeholder="Enter a comment here ...")
-
-        "---"
-        submitted = st.form_submit_button("Save Data")
-        if submitted:
-            period = str(st.session_state["year"]) + "_" + str(st.session_state["month"])
-            incomes = {income: st.session_state[income] for income in incomes}
-            expenses = {expense: st.session_state[expense] for expense in expenses}
-            db.insert_period(period, incomes, expenses, comment)
-            st.success("Data saved!")
+!rm /content/drive/MyDrive/anvilfaces/representations_vgg_face.pkl
+result = ""
+def compare4(image_2):
+  image2 = Image.open(io.BytesIO(image_2.get_bytes()))
+  image2.save("test2.png")
+  result = DeepFace.find(img_path = '/content/test2.png', db_path = '/content/drive/MyDrive/anvilfaces', enforce_detection=False)
+  text2 = ""
+  if(len(result.values) >= 2):
+   text2 = result.identity[1][34:-4]
+  return text2
 
 
-# --- PLOT PERIODS ---
-if selected == "Data Visualization":
-    st.header("Data Visualization")
-    with st.form("saved_periods"):
-        period = st.selectbox("Select Period:", get_all_periods())
-        submitted = st.form_submit_button("Plot Period")
-        if submitted:
-            # Get data from database
-            period_data = db.get_period(period)
-            comment = period_data.get("comment")
-            expenses = period_data.get("expenses")
-            incomes = period_data.get("incomes")
 
-            # Create metrics
-            total_income = sum(incomes.values())
-            total_expense = sum(expenses.values())
-            remaining_budget = total_income - total_expense
-            col1, col2, col3 = st.columns(3)
-            col1.metric("Total Income", f"{total_income} {currency}")
-            col2.metric("Total Expense", f"{total_expense} {currency}")
-            col3.metric("Remaining Budget", f"{remaining_budget} {currency}")
-            st.text(f"Comment: {comment}")
+!rm /content/drive/MyDrive/anvilfaces/representations_vgg_face.pkl
+result = ""
+def compare5(image_2):
+  image2 = Image.open(io.BytesIO(image_2.get_bytes()))
+  image2.save("test2.png")
+  result = DeepFace.find(img_path = '/content/test2.png', db_path = '/content/drive/MyDrive/anvilfaces', enforce_detection=False)
+  text2 = ""
+  if(len(result.values) >= 3):
+   text2 = result.identity[2][34:-4]
+  return text2
 
-            # Create sankey chart
-            label = list(incomes.keys()) + ["Total Income"] + list(expenses.keys())
-            source = list(range(len(incomes))) + [len(incomes)] * len(expenses)
-            target = [len(incomes)] * len(incomes) + [label.index(expense) for expense in expenses.keys()]
-            value = list(incomes.values()) + list(expenses.values())
 
-            # Data to dict, dict to sankey
-            link = dict(source=source, target=target, value=value)
-            node = dict(label=label, pad=20, thickness=30, color="#E694FF")
-            data = go.Sankey(link=link, node=node)
 
-            # Plot it!
-            fig = go.Figure(data)
-            fig.update_layout(margin=dict(l=0, r=0, t=5, b=5))
-            st.plotly_chart(fig, use_container_width=True)
+!rm /content/drive/MyDrive/anvilfaces/representations_vgg_face.pkl
+result = ""
+def compare6(image_2):
+  image2 = Image.open(io.BytesIO(image_2.get_bytes()))
+  image2.save("test2.png")
+  result = DeepFace.find(img_path = '/content/test2.png', db_path = '/content/drive/MyDrive/anvilfaces', enforce_detection=False)
+  text2 = ""
+  if(len(result.values) >= 4):
+   text2 = result.identity[3][34:-4]
+  return text2
+
+
+
+!rm /content/drive/MyDrive/anvilfaces/representations_vgg_face.pkl
+result = ""
+def compare7(image_2):
+  image2 = Image.open(io.BytesIO(image_2.get_bytes()))
+  image2.save("test2.png")
+  result = DeepFace.find(img_path = '/content/test2.png', db_path = '/content/drive/MyDrive/anvilfaces', enforce_detection=False)
+  text2 = ""
+  if(len(result.values) >= 5):
+   text2 = result.identity[4][34:-4]
+  return text2
+
+
+!rm /content/drive/MyDrive/anvilfaces/representations_vgg_face.pkl
+result = ""
+def compare8(image_2):
+  image2 = Image.open(io.BytesIO(image_2.get_bytes()))
+  image2.save("test2.png")
+  result = DeepFace.find(img_path = '/content/test2.png', db_path = '/content/drive/MyDrive/anvilfaces', enforce_detection=False)
+  text2 = ""
+  if(len(result.values) >= 6):
+    text2 = result.identity[5][34:-4]
+  return text2
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
